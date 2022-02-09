0d1fdaf937908e9955f078ef7fb0220c02be0bb9
David Smith <Davidrsmith04@gmail.com>
2022-02-09 17:47:20 +0000

    Functions complete, started func decl vs expr

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -10,7 +10,12 @@ if (hasDriversLicense) console.log('I can drive :D');
 
 const interface = 'Audio';
 const private = 534;
-*/
+
+---------------------
+FUNCTIONS
+---------------------
+
+
 
 function logger() {
     console.log(`My name is Jonas`)
@@ -21,9 +26,42 @@ logger();
 logger();
 
 function fruitProcessor(apples, oranges) {
-    console.log(apples, oranges);
     const juice = `Juice with ${apples} apples and ${oranges} oranges.`
     return juice;
 }
 
 fruitProcessor(5, 0);
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+console.log(fruitProcessor(5, 0)); // Here we did not capture a value in a varible and instead just printed the result directly.
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+
+--------------------------------------
+FUNCTION DECLARATIONS VS. EXPRESSIONS
+--------------------------------------
+
+[DECLARATIONS]
+*/
+function calcAge1(birthYear) {
+    const age = 2037 - birthYear; // Calculate age first
+    return age; // And then we return that value
+}
+
+// The above can be re-written as the following
+
+function calcAge1(birthYear) {
+    return 2037 - birthYear; //This returns a value
+}
+
+const age1 = calcAge1(1991); //We capture that value by saving the function in a variable.
+console.log(age1);
+
+//[EXPRESSIONS]
+
+const calcAge2 = function (birthYear) {
+    return 2037 - birthYear;
+}
