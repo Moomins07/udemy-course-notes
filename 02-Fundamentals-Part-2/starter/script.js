777cc57b134bb24e3ce4a2ae85d2c59037b164ea
David Smith <Davidrsmith04@gmail.com>
2022-02-28 14:59:20 +0000

    Intro to Arrays script/notes updated

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -92,6 +92,10 @@ console.log(yearsUntilRetirement(1991, `Jonas`));
 console.log(yearsUntilRetirement(1980, `Bob`));
 
 
+---------------------------
+FUNCTIONS CALLING FUNCTIONS
+---------------------------
+
 function cutFruitPieces(fruit) {
     return fruit * 4;
 }
@@ -106,6 +110,10 @@ function fruitProcessor(apples, oranges) {
 
 console.log(fruitProcessor(2, 3));
 
+
+---------------------
+REVIEWING FUNCTIONS
+---------------------
 
 const calcAge = function (birthYear) {
     return 2037 - birthYear;
@@ -158,7 +166,7 @@ const friend3 = 'Peter';
 const friends = ['Michael', 'Steven', 'Peter'];
 console.log(friends);
 
-const years = new Array(1991, 1984, 2008, 2020);
+const y = new Array(1991, 1984, 2008, 2020);
 
 console.log(friends[0]);
 console.log(friends[2]);
@@ -168,3 +176,21 @@ console.log(friends[friends.length - 1]);
 
 friends[2] = 'Jay';
 console.log(friends);
+
+const firstName = 'Jonas';
+const jonas = [firstName, 'Schmetmann', 2037 - 1991, 'teacher', friends];
+console.log(jonas);
+
+// Exercise
+const calcAge = function (birthYear) {
+    return 2037 - birthYear;
+}
+const years = [1990, 1967, 2002, 2010, 2018];
+
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length - 1]);
+console.log(age1, age2, age3);
+
+const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length - 1])];
+console.log(ages);
