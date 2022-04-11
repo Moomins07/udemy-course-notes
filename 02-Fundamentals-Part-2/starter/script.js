a5fa079da4a0630ca5d48a0fe957f8f2f272ae72
David Smith <davidrsmith04@gmail.com>
2022-04-11 15:05:23 +0100

    looping arrays, breaking and continuing script/notes finished

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -395,7 +395,12 @@ if (mark.bmi > john.bmi) {
     console.log(`${john.fullName}'s BMI (${Math.round(john.bmi)}) is higher than ${mark.fullName}'s BMI (${Math.round(mark.bmi)})`);
 };
 
-*/
+
+
+---------------------
+ITERATION: THE FOR LOOP
+---------------------
+
 
 //console.log(`Lifting weights repetition 1`);
 
@@ -403,4 +408,53 @@ if (mark.bmi > john.bmi) {
 for (let rep = 1; rep <= 10; rep++) {
     console.log(`Lifting weights repetition  ${rep}`);
 
+}
+
+----------------------------------------
+LOOPING ARRAYS, BREAKING AND CONTINUNIG
+----------------------------------------
+*/
+const jonas = [
+    'Jonas',
+    'Schmedtmann',
+    2037 - 1991,
+    'teacher',
+    ['Michael', 'Peter', 'Steven'],
+    true
+];
+
+const types = [];
+
+for (let i = 0; i < jonas.length; i++) {
+    // reading from jonas array
+    console.log(jonas[i], typeof jonas[i]);
+
+    //filling types array
+    //types[i] = typeof jonas[i];
+    types.push(typeof jonas[i]);
+}
+
+console.log(types);
+
+const years = [1991, 2007, 1969, 2020];
+const ages = [];
+
+for (let i = 0; i < years.length; i++) {
+    ages.push(2037 - years[i]);
+}
+console.log(ages);
+
+// continue and break
+//CONTINUE
+console.log('--- ONLY STRINGS ---')
+for (let i = 0; i < jonas.length; i++) {
+    if (typeof jonas[i] !== 'string') continue;
+    console.log(jonas[i], typeof jonas[i]);
+}
+
+//BREAK
+console.log('--- BREAK WITH NUMBER ---')
+for (let i = 0; i < jonas.length; i++) {
+    if (typeof jonas[i] === 'number') break;
+    console.log(jonas[i], typeof jonas[i]);
 }
