6c2afd4ad59ad3f3bdfe83eaf6c459da4249e642
David Smith <davidrsmith04@gmail.com>
2022-04-13 16:48:09 +0100

    looping backwards and loops in loops scripts/notes finished

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -413,7 +413,7 @@ for (let rep = 1; rep <= 10; rep++) {
 ----------------------------------------
 LOOPING ARRAYS, BREAKING AND CONTINUNIG
 ----------------------------------------
-*/
+
 const jonas = [
     'Jonas',
     'Schmedtmann',
@@ -457,4 +457,28 @@ console.log('--- BREAK WITH NUMBER ---')
 for (let i = 0; i < jonas.length; i++) {
     if (typeof jonas[i] === 'number') break;
     console.log(jonas[i], typeof jonas[i]);
+}
+*/
+
+const jonas = [
+    'Jonas',
+    'Schmedtmann',
+    2037 - 1991,
+    'teacher',
+    ['Michael', 'Peter', 'Steven'],
+    true
+];
+
+
+for (let i = jonas.length - 1; i >= 0; i--) {
+    console.log(i, jonas[i]);
+}
+
+
+for (let exercise = 1; exercise <= 3; exercise++) {
+    console.log(`------- Starting exercise ${exercise} -------`);
+
+    for (let rep = 1; rep <= 5; rep++) {
+        console.log(`Lifting weights repetition ${rep}`);
+    }
 }
