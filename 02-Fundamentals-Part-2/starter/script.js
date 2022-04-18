a0d848298943496000c3741ae336e9c52e99e479
David Smith <davidrsmith04@gmail.com>
2022-04-18 01:25:32 +0100

    Developer skills & editor setup subsec 56 nodejs script/notes

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -515,7 +515,7 @@ while (dice !== 6) {
 ----------------------------------------
 CODING CHALLENGE #4
 ----------------------------------------
-*/
+
 
 const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
 const tips = [];
@@ -550,3 +550,35 @@ const calcAverage = function (arr) {            // arr argument for generic arra
 console.log(calcAverage([2, 3, 7]));
 console.log(calcAverage(totals));
 console.log(calcAverage(tips));
+*/
+
+
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+
+const tips = [];
+
+const totals = [];
+
+const calcTip = function (bill) {
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.20;
+}
+
+for (let i = 0; i <= bills.length - 1; i++) {
+    const tip = calcTip(bills[i]);
+    tips.push(tip);
+    totals.push(tip + bills[i]);
+}
+
+console.log(bills, tips, totals);
+
+
+const calcAverage = function (arr) {
+    let sum = 0;
+    for (let i = 0; i <= arr.length - 1; i++) {
+        sum += arr[i]
+    }
+    return sum / arr.length;
+
+}
+
+console.log(calcAverage(totals));
--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,11 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+const x = '23';
+const birthYear = 2022;
+
+const calcAge = birthYear => 2037 - birthYear;
+
+console.log(calcAge(1981));
+
+//
