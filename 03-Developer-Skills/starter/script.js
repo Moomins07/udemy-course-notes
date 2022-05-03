4226e8717cf19ecafbb64c28833c19b63c45cbc9
David Smith <davidrsmith04@gmail.com>
2022-05-03 15:12:33 +0100

    finished

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -7,7 +7,7 @@ const birthYear = 2022;
 const calcAge = birthYear => 2037 - birthYear;
 
 console.log(calcAge(1981));
-*/
+
 
 // Using Google, StackOverflow and MDN
 
@@ -78,4 +78,48 @@ const calcTempAmplitudeNew = function (t1, t2) {
   return max - min;
 };
 const amplitudeNew = calcTempAmplitudeNew([3, 5, 1], [9, 0, 5]);
+console.log(amplitudeNew);
+*/
+
+const measureKelvin = function () {
+  const measurement = {
+    type: 'temperature',
+    unit: 'celsius',
+
+    // Step 3: FIX
+    //value: Number(prompt('Degrees celsius:')),
+    value: 10,
+  };
+
+  // Step 2: FIND
+  console.table(measurement);
+
+  //console.log(measurement.value);
+  const kelvin = measurement.value + 273;
+  return kelvin;
+};
+
+// Step 1: IDENTIFY
+console.log(measureKelvin());
+
+// using a debugger
+const calcTempAmplitudeBug = function (t1, t2) {
+  const temps = t1.concat(t2);
+  console.log(temps);
+
+  let max = 0;
+  let min = 0;
+
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== 'number') continue;
+    // if curTemp is not a number, skip that iteration
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max, min);
+  return max - min;
+};
+const amplitudeNew = calcTempAmplitudeBug([3, 5, 1], [9, 4, 5]);
+// Step 1: IDENTIFY
 console.log(amplitudeNew);
