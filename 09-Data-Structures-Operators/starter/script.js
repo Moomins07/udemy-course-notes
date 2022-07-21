5355b1c36c3e161ee1310322ca6b24866755b819
David Smith <96678397+Moomins07@users.noreply.github.com>
2022-07-21 18:59:25 +0100

    finished

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -41,7 +41,112 @@ const restaurant = {
       `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}.`
     );
   },
+
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(`Here is your delicious pasta with ${ing1}, ${ing2}, ${ing3}.`);
+  },
+
+  orderPizza: function (mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient);
+    console.log(otherIngredients);
+  },
 };
+// --------------------------------
+// REST OPERATOR
+// --------------------------------
+
+// Destructuring
+
+// SPREAD because on RIGHT side of =
+const arr = [1, 2, ...[3, 4]];
+
+// REST because on LEFT side of =
+const [a, b, ...others] = [1, 2, 3, 4, 5];
+console.log(a, b, others);
+
+// Destructure and rest paramter used
+const [pizza, , risotto, ...otherFood] = [
+  //Spread parameter used to create new array
+  ...restaurant.mainMenu,
+  ...restaurant.starterMenu,
+];
+console.log(pizza, risotto, otherFood);
+
+// Objects
+const { sat, ...weekdays } = restaurant.openingHours;
+console.log(weekdays);
+
+// 2) Functions
+
+const add = function (...numbers) {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) sum += numbers[i];
+  console.log(sum);
+};
+add(2, 3);
+add(5, 3, 7, 2);
+add(8, 2, 5, 3, 2, 1, 4);
+
+const x = [23, 5, 7];
+// Spread operator
+add(...x);
+
+// Rest parameter serves to collect all unused parameteres
+// Everything past 'mushroom' was stored in parameter 'otherIngredients'
+restaurant.orderPizza("mushrooms", "onion", "olives", "spinach");
+restaurant.orderPizza("mushrooms");
+/*
+--------------------------------
+SPREAD OPERATOR
+--------------------------------
+
+const arr = [7, 8, 9];
+const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
+console.log(badNewArr);
+
+const newArray = [1, 2, ...arr];
+console.log(newArray);
+
+console.log(...newArray);
+
+const newMenu = [...restaurant.mainMenu, "Gnocci"];
+console.log(newMenu);
+
+// Copy array
+const mainMenuCopy = [...restaurant.mainMenu];
+
+// Join 2 arrays or more
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+console.log(menu);
+
+// Iterables: arrays, strings, maps, sets. NOT objects.
+const str = "Jonas";
+const letters = [...str, "", "s."];
+console.log(letters);
+console.log(...str);
+
+// Real world example using functions
+const ingredients = [
+  // prompt("Let's make pasta! Ingredient 1?"),
+  // prompt("Ingredient 2?"),
+  // prompt("Ingredient 3?"),
+];
+console.log(ingredients);
+
+restaurant.orderPasta(...ingredients);
+
+// Objects
+const newRestaurant = { foundedIn: 1998, ...restaurant, founder: "Guiseppe" };
+console.table(newRestaurant);
+
+const restaurantCopy = { ...restaurant };
+restaurantCopy.name = "Ristorante Roma";
+console.log(restaurantCopy.name);
+console.log(restaurant.name);
+
+--------------------------------
+DESTRUCTURING OBJECTS
+--------------------------------
 
 restaurant.orderDelivery({
   time: "22:30",
@@ -85,7 +190,7 @@ const {
 } = openingHours;
 console.log(o, c);
 
-/*
+
 --------------------------------
 DESTRUCTURING ARRAYS
 --------------------------------
